75f3d3b4adfe39869836899df945df0ab73ec23e
Djomou _rick <rickkhaki@gmail.com>
2023-12-09 10:52:21 +0100

    debut du projet avec la creation d'un utilisateur et d un compte

---
+++ src/main/java/com/summercoding/bank/backend/entities/Account.java
@@ -0,0 +1,16 @@
+package com.summercoding.bank.backend.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "account_id_seq")
+    @SequenceGenerator(name = "account_id_seq",sequenceName = "account_id_seq",allocationSize = 1)
+
+    private Long idAccount;
+    private  double solde;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
